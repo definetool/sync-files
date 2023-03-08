159861469ebf32b0a478b0ab8670dddf1bd09f00
micty <mictydeng@qq.com>
2023-03-08 15:57:10 +0800

    2.1.0

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const File = require('@definejs/file');
 
 const Resource = require('./lib/Recource');
 const Console = require('./lib/Console');
+const Timer = require('./lib/Timer');
 const Task = require('./modules/Task');
 
 
@@ -19,7 +20,7 @@ function getOutput({ output }) {
 
 
 module.exports = exports = {
-    
+    Timer,
     Task,
 
     parse(config) {
@@ -33,19 +34,34 @@ module.exports = exports = {
         let output = getOutput(config);
         let { dir, cache, patterns, } = config;
         let console = Console.create(`${output}console.log`);
+        let timer = new Timer(console);
+
+        timer.start(`开始任务 >>`.bold);
+
         let resource = Resource.parse(console, { dir, cache, patterns, });
 
         File.writeJSON(`${output}parse.resource.json`, resource);
+
+        timer.stop(`<< 结束任务，耗时: {text}。`.bold);
+
 
         return resource;
     },
 
     sync(config) {
+
         let output = getOutput(config);
         let task = new Task({ ...config, output, });
+        let timer = new Timer(task.console);
+
+        timer.start(`开始任务 >>`.bold);
+
         let { source, target, } = task.parse();
         let compare = task.compare({ source, target, });
         let sync = task.sync({ source, target, compare, });
+
+        timer.stop(`<< 结束任务，耗时: {text}。`.bold);
+
 
         return { source, target, compare, sync, };
     },
